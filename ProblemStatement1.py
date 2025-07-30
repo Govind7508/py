7f4b64ebb9941074453fd486af27246577a45edd
Gobind <rajput19982612@gmail.com>
2025-07-30 22:31:09 +0530

    Question First Done

--- ProblemStatement1.py
+++ ProblemStatement1.py
@@ -5,3 +5,36 @@ a = int(input("Enter your first Number : "))
 b = int(input("Enter your second Number : "))
 c = a + b
 print (f"Total of two number is {c}")
+
+
+
+print()
+print()
+print("Subtract Your Number ")
+print()
+a1 = int(input("Enter your first Number : "))
+b1 = int(input("Enter your second Number : "))
+c1 = a1 - b1
+print (f"Subtract of two number is {c1}")
+
+
+
+print()
+print()
+print("Multiplicate Your Number ")
+print()
+a2 = int(input("Enter your first Number : "))
+b2 = int(input("Enter your second Number : "))
+c2 = a2 * b2
+print (f"Multiplication of two number is {c2}")
+
+
+
+print()
+print()
+print("Divide Your Number ")
+print()
+a3 = int(input("Enter your first Number : "))
+b3 = int(input("Enter your second Number : "))
+c3 = a3 / b3
+print (f"Division of two number is {c3}")
