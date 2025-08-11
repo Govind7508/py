2f843482494849f76b79905666b33d9f83cd8e5d
Gobind <rajput19982612@gmail.com>
2025-08-11 23:05:59 +0530

    Correction in Assignment 4

--- Assignment4.py
+++ Assignment4.py
@@ -1,15 +1,20 @@
-try:
-    file1 = open('sample.txt','r')
-    readFile = file1.read()
-    print(readFile)
-    file1.close()
-except FileNotFoundError : 
-    print("File not Exsist in Directory")
+# try:
+#     file1 = open('sample.txt','r')
+#     readFile = file1.read()
+#     print(readFile)
+#     file1.close()
+# except FileNotFoundError : 
+#     print("File not Exsist in Directory")
     
-sentence = input("Enter a word or sentence to save in file")    
-file2 = open('output.txt','a')
+sentence = input("Enter a word or sentence to save in file : ")    
+file2 = open('output.txt','w')
 file2.write(sentence)
 file2.close()
+
+sentence1 = input("Enter Additional text to append : ")    
+file3 = open('output.txt','a')
+file3.write(sentence1)
+file3.close()
 
 with open('output.txt','r') as file2:
     readFile = file2.read()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # py
+
+in Assignment 4 first I open a Sample.txt for read and Aplly check If file is exsist in folder or in project then read and print file text If no then Show error message Which is handle with try and exception handling
+in second I am write text then append then show the full text 
+
+    
+      
